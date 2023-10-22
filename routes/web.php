9cb431159b113c572e93b872a7675bcf6fb25c48
MaxenceMahieux <pro.maxencemahieux@gmail.com>
2023-10-22 15:26:22 +0200

    Installation de Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\MenuController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\SubmenuController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Resources Routes */
-Route::resource('/', HomeController::class);
-Route::resource('/menu', MenuController::class);
-Route::resource('/submenu', SubmenuController::class);
-Route::resource('/page', PageController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
